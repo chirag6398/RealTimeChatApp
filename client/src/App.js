6da35ba299e381908d9f287838488980a31ef0dc
chirag6398 <agarwalchirag112@gmail.com>
2021-10-09 08:35:13 +0530

    deleted extra folders in react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+import Home from "./components/home/Home";
+export default function App() {
+  return (
+    <div>
+     <Home/>
+    </div>
+  )
+}
+
+
