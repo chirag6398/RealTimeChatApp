6e176ad96e4c85af0481404d0329a9ad5cc8f192
chirag6398 <agarwalchirag112@gmail.com>
2021-10-09 08:30:30 +0530

    initial setup of backened and frontend

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const app=express();
+const path=require("path")
+const port=process.env.PORT || 5000;
+
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+
+
+// app.get("*", function (req, res) {
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+//   });
+
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+})
